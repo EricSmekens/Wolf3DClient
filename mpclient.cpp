01ab4023e922c12620688369248d52ed6de71f2c
Tom <t.zwijgers@gmail.com>
2014-08-31 17:52:34 +0200

    Expanding MPClient

--- mpclient.cpp
+++ mpclient.cpp
@@ -16,18 +16,117 @@
 #define DEFAULT_BUFLEN 512
 #define DEFAULT_PORT "8444"
 
+//int MPClient::startMPClient()
+//{
+//    WSADATA wsaData;
+//    SOCKET ConnectSocket = INVALID_SOCKET;
+//    struct addrinfo *result = NULL,
+//                    *ptr = NULL,
+//                    hints;
+//    char *sendbuf = "this is a test";
+//    char recvbuf[DEFAULT_BUFLEN];
+//    int iResult;
+//    int recvbuflen = DEFAULT_BUFLEN;
+//
+//    // Initialize Winsock
+//    iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+//    if (iResult != 0) {
+//        printf("WSAStartup failed with error: %d\n", iResult);
+//        return 1;
+//    }
+//
+//    ZeroMemory( &hints, sizeof(hints) );
+//    hints.ai_family = AF_UNSPEC;
+//    hints.ai_socktype = SOCK_STREAM;
+//    hints.ai_protocol = IPPROTO_TCP;
+//
+//    // Resolve the server address and port
+//    iResult = getaddrinfo("25.217.237.139", DEFAULT_PORT, &hints, &result);
+//    if ( iResult != 0 ) {
+//        printf("getaddrinfo failed with error: %d\n", iResult);
+//        WSACleanup();
+//        return 1;
+//    }
+//
+//    // Attempt to connect to an address until one succeeds
+//    for(ptr=result; ptr != NULL ;ptr=ptr->ai_next) {
+//
+//        // Create a SOCKET for connecting to server
+//        ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, 
+//            ptr->ai_protocol);
+//        if (ConnectSocket == INVALID_SOCKET) {
+//            printf("socket failed with error: %ld\n", WSAGetLastError());
+//            WSACleanup();
+//            return 1;
+//        }
+//
+//        // Connect to server.
+//        iResult = connect( ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+//        if (iResult == SOCKET_ERROR) {
+//            closesocket(ConnectSocket);
+//            ConnectSocket = INVALID_SOCKET;
+//            continue;
+//        }
+//        break;
+//    }
+//
+//    freeaddrinfo(result);
+//
+//    if (ConnectSocket == INVALID_SOCKET) {
+//        printf("Unable to connect to server!\n");
+//        WSACleanup();
+//        return 1;
+//    }
+//
+//    // Send an initial buffer
+//    iResult = send( ConnectSocket, sendbuf, (int)strlen(sendbuf), 0 );
+//    if (iResult == SOCKET_ERROR) {
+//        printf("send failed with error: %d\n", WSAGetLastError());
+//        closesocket(ConnectSocket);
+//        WSACleanup();
+//        return 1;
+//    }
+//
+//    printf("Bytes Sent: %ld\n", iResult);
+//
+//    // shutdown the connection since no more data will be sent
+//    iResult = shutdown(ConnectSocket, SD_SEND);
+//    if (iResult == SOCKET_ERROR) {
+//        printf("shutdown failed with error: %d\n", WSAGetLastError());
+//        closesocket(ConnectSocket);
+//        WSACleanup();
+//        return 1;
+//    }
+//
+//    // Receive until the peer closes the connection
+//    do {
+//
+//        iResult = recv(ConnectSocket, recvbuf, recvbuflen, 0);
+//        if ( iResult > 0 )
+//            printf("Bytes received: %d\n", iResult);
+//        else if ( iResult == 0 )
+//            printf("Connection closed\n");
+//        else
+//            printf("recv failed with error: %d\n", WSAGetLastError());
+//
+//    } while( iResult > 0 );
+//
+//    // cleanup
+//    closesocket(ConnectSocket);
+//    WSACleanup();
+//
+//    return 0;
+//}
+
 int MPClient::startMPClient()
 {
     WSADATA wsaData;
-    SOCKET ConnectSocket = INVALID_SOCKET;
+    ConnectSocket = INVALID_SOCKET; 
     struct addrinfo *result = NULL,
                     *ptr = NULL,
                     hints;
-    char *sendbuf = "this is a test";
-    char recvbuf[DEFAULT_BUFLEN];
     int iResult;
-    int recvbuflen = DEFAULT_BUFLEN;
-
+    
     // Initialize Winsock
     iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
     if (iResult != 0) {
@@ -77,19 +176,17 @@ int MPClient::startMPClient()
         WSACleanup();
         return 1;
     }
+    
+    return 0;
+}
 
-    // Send an initial buffer
-    iResult = send( ConnectSocket, sendbuf, (int)strlen(sendbuf), 0 );
-    if (iResult == SOCKET_ERROR) {
-        printf("send failed with error: %d\n", WSAGetLastError());
-        closesocket(ConnectSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    printf("Bytes Sent: %ld\n", iResult);
-
-    // shutdown the connection since no more data will be sent
+int MPClient::closeMPClient()
+{
+	int iResult;
+	char recvbuf[DEFAULT_BUFLEN];
+    int recvbuflen = DEFAULT_BUFLEN;
+	
+	// shutdown the connection since no more data will be sent
     iResult = shutdown(ConnectSocket, SD_SEND);
     if (iResult == SOCKET_ERROR) {
         printf("shutdown failed with error: %d\n", WSAGetLastError());
@@ -116,4 +213,20 @@ int MPClient::startMPClient()
     WSACleanup();
 
     return 0;
+}
+
+int MPClient::sendbyMPClient(char *sendbuf)
+{
+	int iResult;
+	
+	// Send an initial buffer
+    iResult = send( ConnectSocket, sendbuf, (int)strlen(sendbuf), 0 );
+    if (iResult == SOCKET_ERROR) {
+        printf("send failed with error: %d\n", WSAGetLastError());
+        closesocket(ConnectSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    printf("Bytes Sent: %ld\n", iResult);
 }
--- mpclient.h
+++ mpclient.h
@@ -1,5 +1,14 @@
+//class MPClient
+//{
+//	public:
+//	  int startMPClient();
+//};
+
 class MPClient
 {
 	public:
 	  int startMPClient();
+	  int closeMPClient();
+	  int sendbyMPClient(char);
+	  SOCKET ConnectSocket;
 };
